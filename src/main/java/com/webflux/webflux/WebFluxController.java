fb98104521b04b3cdf47344d7712740a2edb4ec7
Pan Li <panli@microsoft.com>
2021-08-11 15:44:42 +0800

    Remove netty bug.
    Signed-off-by: Pan Li <panli@microsoft.com>

--- src/main/java/com/webflux/webflux/WebFluxController.java
+++ src/main/java/com/webflux/webflux/WebFluxController.java
@@ -1,8 +1,5 @@
 package com.webflux.webflux;
 
-import io.netty.channel.embedded.EmbeddedChannel;
-import io.netty.util.concurrent.Future;
-import io.netty.util.concurrent.GenericFutureListener;
 import java.util.Random;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,33 +11,10 @@ public class WebFluxController {
 
     private final Random random = new Random();
 
-    private final EmbeddedChannel channel = new EmbeddedChannel();
-
-    @GetMapping("random")
+    @GetMapping
     public String getRandom() {
         int count = random.nextInt(200000);
 
-        int sum = getSum(count);
-
-        return "Get sum " + sum;
-    }
-
-    private int getSum(int count) {
-        int sum = 0;
-        for (int i = 0; i < count; i++) {
-            sum += count;
-
-            GenericFutureListener<Future<Object>> listener = new NonEmptyListener();
-            channel.closeFuture().addListener(listener);
-            channel.closeFuture().removeListener(listener); // not removed if netty instrumentation is on
-        }
-
-        try {
-            channel.close().await();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return sum;
+        return "Get sum " + count;
     }
 }
